9e1ab7420f1d3f2933b39c009127925433cbdbab
João Vitor Teixeira <joaovitorteixeira.dev@gmail.com>
2025-01-25 15:19:42 +1000

    feat: add moving bubbles

--- Scenes/Bubbles/simple_bubble.gd
+++ Scenes/Bubbles/simple_bubble.gd
@@ -1,18 +1,31 @@
-extends RigidBody2D
+extends PathFollow2D
 
 class_name SimpleBubble
 
 var health = 1
-var speed = 1
+var speed = 0.5
+var is_destroyed := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
+
+func finished_path():
+	if is_destroyed:
+		return
+	is_destroyed = true
+	print('destroying it')
+	queue_free()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	
+	progress_ratio += speed * delta
+	
+	if progress_ratio >= 1:
+		finished_path()
+		return
 
 # Destroys the bubble
 func pop() -> void:
