70fd8c887e7c176e8dfe39467cf6a4b89e3f03d9
João Vitor Teixeira <joaovitorteixeira.dev@gmail.com>
2025-01-25 18:36:27 +1000

    feat: add collission detection

--- Scenes/Maps/base_map_1.gd
+++ Scenes/Maps/base_map_1.gd
@@ -11,15 +11,19 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.is_pressed():
-			var towerScene := preload("res://Scenes/Towers/BaseTower.tscn")
-			var tower = towerScene.instantiate()
-			
-			tower.position = event.position
-			add_child(tower)
+			place_tower(event.position)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
+	
+func place_tower(position: Vector2) -> void:
+	var towerScene := preload("res://Scenes/Towers/BaseTower.tscn")
+	var tower = towerScene.instantiate()
+	
+	tower.position = position
+	add_child(tower)
 
 func on_damage_taken(damage_point: int) -> void:
 	if is_game_over:
